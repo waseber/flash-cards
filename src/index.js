2aec44f2ea0434ed19c20511c4a4e3d5284982ce
waseber <yvesgerber@gmail.com>
2020-10-15 20:10:06 -0400

    pushed build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+ReactGA.initialize('UA-180626419-1');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => {
     return(
         <>
             <h1>Make Your Own Flash Cards!</h1>
+            <p>Make it what you want. Whatever you need to study for, add questions and answers and then quiz yourself or friends and family!</p>
         </>
     );
 }
