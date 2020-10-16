568e823c2eaa481a02febb9d14fe63c7e2e25b81
waseber <yvesgerber@gmail.com>
2020-10-16 00:30:09 -0400

    fixes redirect issue

---
+++ precache-manifest.63caf2282cd1af3d4858fc99a8c2c58e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c15bc13370197931a15b39c4115fafa",
+    "url": "/flash-cards/index.html"
+  },
+  {
+    "revision": "e5592a7b8c5c170162a2",
+    "url": "/flash-cards/static/css/main.36036d6c.chunk.css"
+  },
+  {
+    "revision": "ebc01773c8f128eed17c",
+    "url": "/flash-cards/static/js/2.71a90202.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/flash-cards/static/js/2.71a90202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5592a7b8c5c170162a2",
+    "url": "/flash-cards/static/js/main.7c5104b0.chunk.js"
+  },
+  {
+    "revision": "316700a5680da7418d9d",
+    "url": "/flash-cards/static/js/runtime-main.dfd2518f.js"
+  }
+]);
