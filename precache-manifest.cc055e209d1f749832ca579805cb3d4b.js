ee9b37ad641ccd33dd180e94061e9df7977dac6c
waseber <yvesgerber@gmail.com>
2020-10-19 14:08:30 -0400

    adds hash router to handle reloads

--- precache-manifest.cc055e209d1f749832ca579805cb3d4b.js
+++ precache-manifest.cc055e209d1f749832ca579805cb3d4b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c15bc13370197931a15b39c4115fafa",
+    "revision": "58fef9663023c58cfaa1b15c0ae82d1d",
     "url": "/flash-cards/index.html"
   },
   {
-    "revision": "e5592a7b8c5c170162a2",
+    "revision": "87478c1dbcae51d46523",
     "url": "/flash-cards/static/css/main.36036d6c.chunk.css"
   },
   {
-    "revision": "ebc01773c8f128eed17c",
-    "url": "/flash-cards/static/js/2.71a90202.chunk.js"
+    "revision": "b54371f7632cd2df7d19",
+    "url": "/flash-cards/static/js/2.378706f4.chunk.js"
   },
   {
     "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
-    "url": "/flash-cards/static/js/2.71a90202.chunk.js.LICENSE.txt"
+    "url": "/flash-cards/static/js/2.378706f4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5592a7b8c5c170162a2",
-    "url": "/flash-cards/static/js/main.7c5104b0.chunk.js"
+    "revision": "87478c1dbcae51d46523",
+    "url": "/flash-cards/static/js/main.f43a1df8.chunk.js"
   },
   {
     "revision": "316700a5680da7418d9d",
